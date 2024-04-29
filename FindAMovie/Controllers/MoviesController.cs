aabc9a6ecc4f58b02bebee7346e7ea3fbf8f9995
David Phelps <DPhelps@syslogicinc.com>
2024-04-29 10:35:22 -0500

    Add project files.

---
+++ FindAMovie/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FindAMovie.Models;
+
+namespace FindAMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Iron Man"
+            };
+
+            //return View(movie);
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+    }
+}
---
+++ FindAMovie/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindAMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+    }
+}

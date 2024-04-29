2f6b76a26f76235133ce94e68b23dc3eba2c16e4
David Phelps <DPhelps@syslogicinc.com>
2024-04-29 11:38:40 -0500

    Added routing for Index method

--- FindAMovie/Controllers/MoviesController.cs
+++ FindAMovie/Controllers/MoviesController.cs
@@ -17,8 +17,34 @@ namespace FindAMovie.Controllers
                 Name = "Iron Man"
             };
 
-            //return View(movie);
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            return View(movie);
+
+            // example of a redirect with parameter values
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // nullable types: all of the following do the same thing
+        // int?
+        // Nullable<int>
+        // Nullable<System.Int32>
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }

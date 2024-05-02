8b10c5a468ebaad29050d9275177ff05fc9b86e0
David Phelps <DPhelps@syslogicinc.com>
2024-05-02 17:34:59 -0500

    Fleshed out Details view and added Name to MembershipType

--- FindAMovie/Controllers/CustomersController.cs
+++ FindAMovie/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using FindAMovie.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,14 +25,14 @@ namespace FindAMovie.Controllers
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
